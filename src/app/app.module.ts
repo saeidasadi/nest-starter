66a49400148b3faa54d3b662c0992493c5e89bdd
Saeid Asadi <saeidasadi1995@gmail.com>
2021-01-24 11:19:32 +0330

    add components module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "../config/config.module";
 import { DatabaseModule } from "../database/database.module";
+import { ComponentsModule } from "../components/components.module";
 
 
 @Module({
   imports: [
     ConfigModule,
     DatabaseModule,
+    ComponentsModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/components/components.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+
+@Module({
+  imports: [
+    //
+  ],
+  providers: [
+    //
+  ],
+  exports: [
+    //
+  ],
+})
+export class ComponentsModule {
+
+}
