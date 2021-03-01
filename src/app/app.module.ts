cc482966da13dfcc3255b232a689d4dcc3e595d4
Saeid Asadi <saeidasadi1995@gmail.com>
2021-03-01 13:31:27 +0330

    add public folder for static assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from "../config/config.module";
 import { DatabaseModule } from "../database/database.module";
 import { ComponentsModule } from "../components/components.module";
-
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
   imports: [
     ConfigModule,
     DatabaseModule,
     ComponentsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'public'),
+    }),
   ],
   controllers: [],
   providers: [],
