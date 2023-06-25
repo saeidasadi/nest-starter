bcb0d6ac6a6b5cc761ed79945cd1094a31aa51ad
Saeid Asadi <saeidasadi1995@gmail.com>
2023-06-25 08:39:48 +0330

    add app key configuration

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
+  key: process.env.APP_KEY,
+
   port: parseInt(process.env.APP_PORT, 10) || 3000,
 
   timezone: process.env.APP_TIMEZONE || 'UTC',
